564895785c695e194737dd14e0d1d9b968b3622c
Ale478 <alemusicmolin@gmail.com>
2022-03-25 00:53:54 -0300

    Adds test main

--- portfolio/tests/test_main.py
+++ portfolio/tests/test_main.py
@@ -1,13 +1,20 @@
-import unittest, mock
+import unittest, mock, patch
+from unittest import result
 from portfolio import main
-from datetime import datetime
-from portfolio.core.stock import Stock
 from portfolio.tests.testFixtures import invalid_format
 
 
 class TestInputDate(unittest.TestCase):
-    
+
+    @patch('builtins.input', return_value="2019/1/1")
+    def test_date_invalid(self, mock_input):
+        date = main()
+       
+
+    """
     def test_input_date(self):        
         with self.assertRaises(ValueError):
             invalid_format(True)
-
+    """
+ 
+    
