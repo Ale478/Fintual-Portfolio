b30018fe2d93fd4e2836dc3755cc0c3e66bb6f32
Ale478 <alemusicmolin@gmail.com>
2022-04-01 00:38:35 -0300

    Delete test_main

--- portfolio/tests/testFixtures.py
+++ portfolio/tests/testFixtures.py
@@ -7,7 +7,5 @@ def price_not_found():
     raise PriceNotFoundError('Price for stock {stock_name} not found')
 
 
-def invalid_format():
-    raise ValueError('Invalid format entered :(, please try again!')
 
 
