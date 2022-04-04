fb81388c2bf9c4cf14e6c1495655c62b4d5bde35
Ale478 <alemusicmolin@gmail.com>
2022-04-04 00:25:52 -0300

    Hotfix

--- main.py
+++ main.py
@@ -26,59 +26,59 @@ s2 = Stock(
 
 def main():
 
-    try: 
-        print(f"\nChoose what you want to do. Options are:\n"
-           "\n"
-           "\t**************************************************************\n"
-           "\t* a. Profit of the Portfolio                                 *\n"
-           "\t* b. Annualized Profitability of the Portfolio               *\n"
-           "\t* c. All                                                     *\n"
-           "\t* q. Quit                                                    *\n"
-           "\t**************************************************************\n")
-        option = input("\n Please enter your choice: ")
+ 
+    print(f"\nChoose what you want to do. Options are:\n"
+       "\n"
+       "\t**************************************************************\n"
+       "\t* a. Profit of the Portfolio                                 *\n"
+       "\t* b. Annualized Profitability of the Portfolio               *\n"
+       "\t* c. All                                                     *\n"
+       "\t* q. Quit                                                    *\n"
+       "\t**************************************************************\n")
+    option = input("\n Please enter your choice: ")
     
-    except ValueError:
-        print(f"Invalid option entered :(, please try again!")
-        return
-
-
-    while(option != 'q'):
-        try:
-
-            date_entry = input('\n Please enter a first date in YYYY-MM-DD format: ')
-            date1 = datetime.strptime(date_entry, "%Y-%m-%d")
-
-            date_entry = input('\n Please enter a second date in YYYY-MM-DD format: ')
-            date2 = datetime.strptime(date_entry, "%Y-%m-%d")
+    if ((option == 'a') or (option =='b') or (option =='c') or (option =='q')):
         
-        except ValueError:
-            print(f"Invalid format entered :(, please try again!")
-            return
+        while(option != 'q'):
+            try:
 
+                date_entry = input('\n Please enter a first date in YYYY-MM-DD format: ')
+                date1 = datetime.strptime(date_entry, "%Y-%m-%d")
 
-        myPortfolio = Portfolio([s1, s2]) 
-        profit, annualized = myPortfolio.profit(date1, date2)
-        txt = "The annualized profitability obtained was  {:.0%}"
-        
-        
-
-        if option == 'a':
-            print(f"\n The profit obtained was {profit}")
-            option = input("\n Please enter your choice: ")  
-
-        elif option == 'b':
-            print(f"\n", txt.format(annualized))
-            option = input("\n Please enter your choice: ")
+                date_entry = input('\n Please enter a second date in YYYY-MM-DD format: ')
+                date2 = datetime.strptime(date_entry, "%Y-%m-%d")
             
+            except ValueError:
+                print(f"Invalid format entered :(, please try again!")
+                return
+
         
-        elif option == 'c':
-            print(f"\n The profit obtained was {profit}")
-            print(f"\n", txt.format(annualized))
-            option = input("\n Please enter your choice: ")
+            myPortfolio = Portfolio([s1, s2]) 
+            profit, annualized = myPortfolio.profit(date1, date2)
+            txt = "The annualized profitability obtained was  {:.2%}"
+
+            if option == 'a':
+                print(f"\n The profit obtained was {profit}")
+                option = input("\n Please enter your choice: ")  
+
+            elif option == 'b':
+                print(f"\n", txt.format(annualized))
+                option = input("\n Please enter your choice: ")
+                
+                
+            
+            elif option == 'c':
+                print(f"\n The profit obtained was {profit}")
+                print(f"\n", txt.format(annualized))
+                option = input("\n Please enter your choice: ")
         
     if option == 'q':
             print(f"\n Thanks for use the portfolio :) \n")
             quit()
 
+
+    else:    
+        print(f"Invalid option entered :(, please try again!")
+        
 
 main()
--- portfolio/core/portfolio.py
+++ portfolio/core/portfolio.py
@@ -1,4 +1,8 @@
-from portfolio.core.stock import PriceNotFoundError, Stock
+from time import strptime
+from portfolio.core.stock import PriceNotFoundError
+from dateutil.relativedelta import relativedelta
+from datetime import timedelta, datetime
+
 
 
 
@@ -10,12 +14,12 @@ class Portfolio:
 
     # Receives two dates and returns the benefit between those dates
 
-    def profit(self, date_1, date_2):
-        
+    
+    def profit(self, date_1, date_2):        
         profits = 0
         init_value = 0
         final_value = 0
-        date_interval = date_2.year - date_1.year 
+        date_diff = (date_2 - date_1).days
         annualized_return = 0
         
         for stock in self.stocks:
@@ -25,11 +29,9 @@ class Portfolio:
                 init_value += date1
                 final_value += date2
                 profits += date2 - date1
-                annualized_return = ((final_value/init_value)-1)*(1/date_interval)
+                annualized_return = ((final_value/init_value)-1)*(365/date_diff)
             except PriceNotFoundError as e:
                 print("\n", e)
             
-
-        
 
         return(profits, annualized_return)
